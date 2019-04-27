3837de86f5f8dc292048cb86a52c46b257d3727c
Mark <mprouty@mycomputerbooks.com>
2019-04-26 19:08:56 -0500

    Factory and Seed

---
+++ database/factories/ModelLikeFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Like;
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Like::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return \App\User::all()->random();
+        }
+    ];
+});
---
+++ database/factories/ModelReplyFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Reply;
+use Faker\Generator as Faker;
+use App\Model\Question;
+
+$factory->define(App\Model\Reply::class, function (Faker $faker) {
+    return [
+        'body' => $faker->text,
+        'question_id' => function () {
+            return Question::all()->random();
+        },
+        'user_id' => function () {
+            return \App\User::all()->random();
+        },
+    ];
+});
