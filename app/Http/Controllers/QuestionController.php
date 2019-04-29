50430b746f223825ab114cc50135f2768f773ad6
Mark <mprouty@mycomputerbooks.com>
2019-04-29 14:38:18 -0500

    CRUD for Question along with Question Resource Transformation

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Model\Question;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\QuestionResource;
+use App\Http\Requests\QuestionRequest;
 
 class QuestionController extends Controller
 {
@@ -14,17 +17,8 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        //return Question::latest()->get();
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
@@ -33,9 +27,22 @@ class QuestionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
     public function store(Request $request)
     {
-        //
+
+        // $question = new Question;
+        // $question->title = $request->title;
+        // $question->slug = $request->slug;
+        // $question->body = $request->body;
+        // $question->category_id = $request->category_id;
+        // $question->user_id = $request->user_id;
+        // $question->save();
+        //added protected $guarded = [] to Question.php;
+        //auth()->user()->question()->create($request->all());
+
+        Question::create($request->all());
+        return response('Created', Response::HTTP_CREATED);
     }
 
     /**
@@ -46,18 +53,9 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
+        //added getRouteKeyName in Question.php
+        // return $question;
+        return new QuestionResource($question);
     }
 
     /**
@@ -80,6 +78,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,15 @@ use App\User;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    //protected $fillable = ['title','slug','body','user_id','category_id'];
+
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -18,6 +27,11 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("/question/$this->slug");
     }
 
 }
